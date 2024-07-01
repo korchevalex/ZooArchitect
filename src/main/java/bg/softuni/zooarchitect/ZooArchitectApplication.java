0b51af2a5d087197d443d576e4bc830158a57e53
korchevalex <alexkorchev123456@gmail.com>
2024-07-01 15:59:35 +0300

    Added HTML template

---
+++ src/main/java/bg/softuni/zooarchitect/ZooArchitectApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.zooarchitect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooArchitectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZooArchitectApplication.class, args);
+    }
+
+}
