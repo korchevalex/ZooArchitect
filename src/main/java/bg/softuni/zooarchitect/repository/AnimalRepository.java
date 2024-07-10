73ede7dcaea9d3dbb6f8031ca9b0fe12e5ae8564
korchevalex <alexkorchev123456@gmail.com>
2024-07-10 13:51:44 +0300

    Added a plan for future pages, repositories and services

--- src/main/java/bg/softuni/zooarchitect/repository/AnimalRepository.java
+++ src/main/java/bg/softuni/zooarchitect/repository/AnimalRepository.java
@@ -1,9 +1,9 @@
 package bg.softuni.zooarchitect.repository;
 
-import bg.softuni.zooarchitect.model.entity.Zoo;
+import bg.softuni.zooarchitect.model.entity.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ZooRepository extends JpaRepository<Zoo, Long> {
+public interface AnimalRepository extends JpaRepository<Animal, Long> {
 }
--- src/main/java/bg/softuni/zooarchitect/repository/CommentRepository.java
+++ src/main/java/bg/softuni/zooarchitect/repository/CommentRepository.java
@@ -1,9 +1,9 @@
 package bg.softuni.zooarchitect.repository;
 
-import bg.softuni.zooarchitect.model.entity.Zoo;
+import bg.softuni.zooarchitect.model.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ZooRepository extends JpaRepository<Zoo, Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
